29ad916dc7b15dd08f5e3af72d07b5542008ba85
Adriel Ordonez <ordonez.codes@gmail.com>
2019-09-18 20:59:37 -0700

    using redux/reducers 09182019 2100

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -13,7 +13,10 @@ export class Main extends React.Component {
                     <div className="col-xs-12">
                         <button
                             className="btn btn-primary"
-                            onClick={() => this.props.changeUsername('Anna')}>Change the Username</button>
+                            onClick={() => this.props.changeUsername('Akiko')}
+                        >
+                            Change the Username
+                        </button>
                     </div>
                 </div>
             </div>
